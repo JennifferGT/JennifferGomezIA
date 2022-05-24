056dc040d97709cb0ba619b93dba7962320a9ba9
JennifferGomez <jenniffer.jg59@gmail.com>
2022-05-24 00:47:32 -0500

    Menú de estado del café

--- cafetera.py
+++ cafetera.py
@@ -11,4 +11,8 @@ def cafe():
     #Crea una función que verifica el lugar de la cafetera
     # Si la cafetera está en la cocina se debe ingresar el número 1.
     if opcionMe == '1':
+        #Imprime el menú de estado del café si está apagado se ingresa 0 y si está encendido se ingresa 1
+        print("Seleccione el estado de la cafetera: ")
+        print("0. Apagado")
+        print("1. Encendido")
 cafe()

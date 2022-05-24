0ede07a43824d49bb0140d64b695d04331e838c1
JennifferGomez <jenniffer.jg59@gmail.com>
2022-05-24 00:44:23 -0500

    Corrección de errores

--- cafetera.py
+++ cafetera.py
@@ -1,6 +1,8 @@
-def menu():
+#Muestra un menú detallando los lugares de la cafetera.
+def cafe():
     print("Seleccione el lugar de la cafetera: ")
     print("1. Cocina")
     print("2. Oficina")
     print("3. Restaurante")
-    menu()
+    #Inicializamos en 0 el número de movimentos que realiza la cafetera.
+cafe()

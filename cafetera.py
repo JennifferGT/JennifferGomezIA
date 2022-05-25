0303943699eca7ed259399f8a795a2cc58b7bd50
JennifferGomez <jenniffer.jg59@gmail.com>
2022-05-25 18:46:50 -0500

    comentario

--- cafetera.py
+++ cafetera.py
@@ -1,12 +1,13 @@
 #Muestra un menú detallando los lugares de la cafetera.
 def cafe():
+    #Inicializamos en 0 el número de movimentos que realiza la cafetera.
     aumento = 0;
     while True:
         print("Seleccione el lugar de la cafetera: ")
         print("1. Cocina")
         print("2. Oficina")
         print("3. Restaurante")
-        #Inicializamos en 0 el número de movimentos que realiza la cafetera.
+        
         
         #Ingresar por medio de número el lugar de la cafetera que desea encender.
         opcionMe = input("Insertar el lugar de la cafetera: ")

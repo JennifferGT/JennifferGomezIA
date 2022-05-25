6f1d539f8e2ba018ca412c0138d9a5aa25b4a1f0
JennifferGomez <jenniffer.jg59@gmail.com>
2022-05-25 18:44:45 -0500

    bucle

--- cafetera.py
+++ cafetera.py
@@ -10,6 +10,7 @@ def cafe():
     opcionMe = input("Insertar el lugar de la cafetera: ")
     #Crea una función que verifica el lugar de la cafetera
     # Si la cafetera está en la cocina se debe ingresar el número 1.
+   
     if opcionMe == '1':
         #Imprime el menú de estado del café si está apagado se ingresa 0 y si está encendido se ingresa 1
         print("Seleccione el estado de la cafetera: ")
